c8ab4a4850db7216e5d0e3fe557182e10e341dab
Gary Crowe <49523637+gary-crowe@users.noreply.github.com>
2021-04-15 17:26:16 +0100

    Update harden_to_level1.bash
    Added ask for password flag so we don;t need to mess with visudo

--- harden_to_level1.bash
+++ harden_to_level1.bash
@@ -2,4 +2,4 @@
 # Script to run harden script against target machines in "inv" file.
 # Assume you have set-up your ssh keys.
 
-ansible-playbook -vi inv -e  deployment_vars=CIS7_1exceptions.yml RHEL7-CIS_Benchmark_level1.yml
+ansible-playbook -vi inv  --ask-become-pass -e  deployment_vars=CIS7_1exceptions.yml RHEL7-CIS_Benchmark_level1.yml
