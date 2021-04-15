27955c7a5186411043b138403641d66b72b9814b
Gary Crowe <49523637+gary-crowe@users.noreply.github.com>
2021-04-15 17:26:39 +0100

    Update harden_to_level2.bash

--- harden_to_level2.bash
+++ harden_to_level2.bash
@@ -2,4 +2,4 @@
 # Script to run harden script against target machines in "inv" file.
 # Assume you have set-up your ssh keys.
 
-ansible-playbook -vi inv -e  deployment_vars=CIS7_2exceptions.yml RHEL7-CIS_Benchmark_level2.yml
+ansible-playbook -vi inv  --ask-become-pass  -e  deployment_vars=CIS7_2exceptions.yml RHEL7-CIS_Benchmark_level2.yml
